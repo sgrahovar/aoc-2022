1fd896c66d722e45d3c8d00e1df1c895279a3c92
sgrahovar <sanjin.grahovar@gmail.com>
2022-12-03 19:08:07 +0100

    Solved Day 3, part 1

--- aoc-2022/Day3/Day3.cpp
+++ aoc-2022/Day3/Day3.cpp
@@ -51,12 +51,7 @@ public:
         int sumOfPriorities = 0;
         
         for(int i = 0; i < input1.size(); i++){
-            std::cout << input1[i] << std::endl << input1[i].substr(0, input1[i].size()/2) << " " << input1[i].substr(input1[i].size()/2, input1[i].size()) << std::endl;
-        }
-        std::cout << "Result: " << std::endl;
-        
-        
-        for(int i = 0; i < input1.size(); i++){
+            tmpArray.clear();
             for(int j = 0; j < input1[i].size() / 2; j++){
                 for(int k = input1[i].size() / 2; k <= input1[i].size(); k++){
                     if(input1[i][j] == input1[i][k]){
@@ -64,21 +59,18 @@ public:
                         // j++;
                         if (std::find(tmpArray.begin(), tmpArray.end(), input1[i][j]) == tmpArray.end()){
                             tmpArray.push_back(input1[i][j]);
-                            std::cout << "Same - " << input1[i][j] << std::endl;
+                            // std::cout << "Same - " << input1[i][j] << std::endl;
                             
                             if(isupper(input1[i][j])){
-                                std::cout << "Upper case - " << ((int(input1[i][j])) - 38) << std::endl;
+                                // std::cout << "Upper case - " << ((int(input1[i][j])) - 38) << std::endl;
                                 sumOfPriorities += ((int(input1[i][j])) - 38);
-                                // break;
                             }
                             else {
-                                std::cout << "Lower case - " << ((int(input1[i][j])) - 96) << std::endl;
+                                // std::cout << "Lower case - " << ((int(input1[i][j])) - 96) << std::endl;
                                 sumOfPriorities += ((int(input1[i][j])) - 96);
-                                // break;
                             }
                             
-                            std::cout << "Sum: " << sumOfPriorities << std::endl;
-                            // j++;
+                            // std::cout << "Sum: " << sumOfPriorities << std::endl;
                             break;
                         }
                         
